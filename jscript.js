bc01d645d6a47b8043fd256e9a678aa1b77cbdbf
Kgoomba <Jdraggoo73@gmail.com>
2022-10-05 10:17:10 -0400

    Created visuals for the calculator

---
+++ jscript.js
@@ -0,0 +1,41 @@
+const container = document.querySelector("#container");
+
+container.classList.add("gridCon");
+
+function add(a, b){
+  return sum = a + b
+};
+
+function subtract(a, b){
+  return difference = a - b
+};
+
+function multiply(a, b){
+  return product = a * b
+};
+
+function divide(a, b){
+  return quotient = a/b
+};
+
+/*function operate(a, b){
+  switch (idk) {
+    case "add":
+      add(a, b), alert(sum);
+      break;
+    case "subtract":
+      subtract(a, b), alert(difference);
+      break;
+    case "multiply":
+      multiply(a, b), alert(product);
+      break;
+    case "divide":
+      divide(a, b), alert(quotient);
+      break;
+    default:
+      return "ERROR"
+  }
+};
+operate(2, 2);*/
+
+let numArray = newArray[0]
