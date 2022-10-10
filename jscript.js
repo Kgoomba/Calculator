afe000dde70c2227b3f89e01275b4c3f33037087
Kgoomba <Jdraggoo73@gmail.com>
2022-10-10 13:17:51 -0400

    Added javascript junctionality to the caulculator.

--- jscript.js
+++ jscript.js
@@ -1,41 +1,135 @@
-const container = document.querySelector("#container");
-
-container.classList.add("gridCon");
+const numBtns = document.querySelectorAll(".numBtn");
+const bottomScreen = document.querySelector(".bottomScreen");
+const topScreen = document.querySelector(".topScreen");
+const equalBtn = document.querySelector(".equalBtn")
+const operateBtns = document.querySelectorAll(".operateBtn")
+const clearBtn = document.querySelector("#btnClear")
+const ceBtn = document.querySelector(".clearBtn")
 
 function add(a, b){
-  return sum = a + b
-};
+  result = a + b;
+  console.log(result);
+  tempNum = result
+}
 
 function subtract(a, b){
-  return difference = a - b
+  result = a - b;
+  console.log(result);
+  tempNum = result
 };
 
 function multiply(a, b){
-  return product = a * b
+  result = a * b;
+  console.log(result);
+  tempNum = result
 };
 
 function divide(a, b){
-  return quotient = a/b
+  result = a/b;
+  console.log(result);
+  tempNum = result
 };
 
-/*function operate(a, b){
-  switch (idk) {
-    case "add":
-      add(a, b), alert(sum);
+function operate(operator){
+  let a = tempNum
+  let b = parseFloat(bottomScreen.textContent)
+  switch (operator) {
+    case "+":
+      add(a, b);
+      bottomScreen.textContent = result;
       break;
-    case "subtract":
-      subtract(a, b), alert(difference);
+    case "-":
+      subtract(a, b);
+      bottomScreen.textContent = result;
       break;
-    case "multiply":
-      multiply(a, b), alert(product);
+    case "x":
+      multiply(a, b);
+      bottomScreen.textContent = result;
       break;
-    case "divide":
-      divide(a, b), alert(quotient);
+    case "÷":
+      divide(a, b);
+      bottomScreen.textContent = result;
       break;
     default:
       return "ERROR"
   }
 };
-operate(2, 2);*/
 
-let numArray = newArray[0]
+let tempNum = 0
+let tempOperator = ""
+let result = 0
+bottomScreen.textContent = "0"
+topScreen.textContent = ""
+
+numBtns.forEach(btn => {
+  btn.addEventListener("click", (e) => {
+    let btnNum = e.target.textContent
+    let btnId = e.target.id
+    if (bottomScreen.textContent === "0"){
+      bottomScreen.textContent = ""
+    } if (btnId === "deciBtn" && bottomScreen.textContent.includes(".")){
+      return 
+    } else {
+      //do nothing
+    }
+    bottomScreen.textContent += btnNum
+  })
+})
+
+operateBtns.forEach(btn => {
+  btn.addEventListener("click", (e) => {
+    let operator = e.target.textContent
+     if (topScreen.textContent.includes("=")){
+      tempOperator = operator
+      updateScreen(operator)
+    } else if (topScreen.textContent === ""){
+      tempNum = parseFloat(bottomScreen.textContent);
+      topScreen.textContent = bottomScreen.textContent;
+      topScreen.textContent += operator;
+      bottomScreen.textContent = "";
+      tempOperator = operator
+    } else if (!topScreen.textContent.includes(operator)){
+      operator = tempOperator
+      operate(operator)
+      operator = e.target.textContent
+      tempOperator = operator
+      updateScreen(operator)
+    } else if (topScreen.textContent.includes(operator)){
+      operate(operator)
+      updateScreen(operator)
+      tempOperator = operator
+    }
+  })
+});
+
+equalBtn.addEventListener("click", (e) => {
+  if (topScreen.textContent === ""){
+    // do nothing
+  } else if(!topScreen.textContent.includes("=")) {
+    topScreen.textContent += bottomScreen.textContent
+    console.log(tempOperator)
+    operator = tempOperator
+    operate(operator)
+    topScreen.textContent += "="
+  } else {
+    // do nothing
+  }
+})
+
+clearBtn.addEventListener("click", (e) =>{
+  topScreen.textContent = ""
+  bottomScreen.textContent = "0"
+  tempNum = 0
+})
+
+ceBtn.addEventListener("click", (e) =>{
+  bottomScreen.textContent = "0"
+})
+
+
+
+function updateScreen(operator){
+  topScreen.textContent = result
+  topScreen.textContent += operator
+  bottomScreen.textContent = ""
+}
